386c398b19f27deb0cd8e95979306bd661823e6b
Josh Argent <josh@vanillaworld.org>
2014-06-03 08:58:36 +0100

    Added main class

---
+++ NicePM/src/com/joshargent/NicePM/NicePM.java
@@ -0,0 +1,17 @@
+package com.joshargent.NicePM;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NicePM extends JavaPlugin {
+	
+	public void onEnable()
+	{
+		
+	}
+	
+	public void onDisable()
+	{
+		
+	}
+
+}
