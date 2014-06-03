717628f8629b3dd9db1a2cc5cdc3002aff5c1379
Josh Argent <josh@vanillaworld.org>
2014-06-03 09:24:04 +0100

    Added plugin.yml and default config.yml

--- NicePM/src/com/joshargent/NicePM/NicePM.java
+++ NicePM/src/com/joshargent/NicePM/NicePM.java
@@ -1,12 +1,20 @@
 package com.joshargent.NicePM;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class NicePM extends JavaPlugin {
+public class NicePM extends JavaPlugin implements Listener {
 	
 	public void onEnable()
 	{
+		if(!this.getConfig().isSet("message-to"))
+		{
+			this.saveDefaultConfig();
+		}
 		
+		getServer().getPluginManager().registerEvents(this, this);
 	}
 	
 	public void onDisable()
@@ -14,4 +22,10 @@ public class NicePM extends JavaPlugin {
 		
 	}
 
+	public boolean onCommand(final CommandSender sender, Command cmd, String label, String[] args)
+	{
+		
+		return false;
+	}
+	
 }
